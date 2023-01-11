f7a4f4e5ef4973fa8a3920633f0de2b3ac77c714
SoundOrion <107441051+SoundOrion@users.noreply.github.com>
2023-01-11 15:47:29 +0900

    Add files via upload
    https://learn.microsoft.com/ja-jp/aspnet/core/blazor/components/cascading-values-and-parameters?view=aspnetcore-7.0

---
+++ BlazorAppCascading/BlazorAppCascading/Cascade/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace BlazorAppCascading.Cascade
+{
+    public class Message
+    {
+        private string _name;
+
+        public Message()
+        {
+            _name = "Guest";
+        }
+
+        public Message(string name)
+        {
+            _name = name;
+        }
+
+        public string GetText()
+        {
+            return $"Hello {_name}!!";
+        }
+    }
+}
